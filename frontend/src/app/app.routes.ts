85776eb42742322373286615b0da4f106c5851e7
Monky033 <juanimignacio@gmail.com>
2026-06-03 19:35:20 -0300

    correcion de rutas a componentes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
-
-import { CajaComponent } from './pages/caja/caja.component';
-import { MenuComponent } from './pages/menu/menu.component';
-import { NuevoProductoComponent } from './pages/nuevo-producto/nuevo-producto.component';
+import { Caja } from './caja/caja';
+import { Menu } from './menu/menu';
+import { NuevoProducto } from './nuevo-producto/nuevo-producto';
 
 export const routes: Routes = [
-  { path: 'caja', component: CajaComponent },
-  { path: 'menu', component: MenuComponent },
-  { path: 'nuevo-producto', component: NuevoProductoComponent }
+  { path: 'caja', component: Caja },
+  { path: 'menu', component: Menu },
+  { path: 'nuevo-producto', component: NuevoProducto }
 ];
