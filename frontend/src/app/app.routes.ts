578b87dd6709523c5d84bfef0c8aa24b9e77489a
Monky033 <juanimignacio@gmail.com>
2026-06-03 19:22:50 -0300

    Agrego componentes caja, menu y nuevo-producto

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CajaComponent } from './pages/caja/caja.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { NuevoProductoComponent } from './pages/nuevo-producto/nuevo-producto.component';
+
+export const routes: Routes = [
+  { path: 'caja', component: CajaComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'nuevo-producto', component: NuevoProductoComponent }
+];
