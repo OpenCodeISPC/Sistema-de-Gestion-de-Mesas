615690a110e375cf9aeb95f25a8a90b4e3385272
Mariano Casarino <mariano.e.casarino@gmail.com>
2026-06-03 22:25:40 -0300

    Creación de componentes mesas, servicios y pedidos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { Caja } from './caja/caja';
 import { Menu } from './menu/menu';
 import { NuevoProducto } from './nuevo-producto/nuevo-producto';
+import { Mesas } from './features/mesas/mesas';
+import { Servicios } from './features/servicios/servicios';
+import { Pedidos } from './features/pedidos/pedidos';
 
 export const routes: Routes = [
   { path: 'caja', component: Caja },
   { path: 'menu', component: Menu },
-  { path: 'nuevo-producto', component: NuevoProducto }
+  { path: 'nuevo-producto', component: NuevoProducto },
+  { path: 'mesas', component: Mesas},
+  { path: 'servicios', component: Servicios},
+  { path: 'pedidos', component: Pedidos}
 ];
---
+++ frontend/src/app/features/mesas/mesas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mesas',
+  imports: [],
+  templateUrl: './mesas.html',
+  styleUrl: './mesas.css',
+})
+export class Mesas {
+
+}
