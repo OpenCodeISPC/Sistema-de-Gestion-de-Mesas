fb674c8debce03f11b8f6ac6440d0940ec528336
AAMinoldo <minoldoaldo@gmail.com>
2026-08-24 20:33:47 -0300

    feat: ruteo de login a registro, olvide la contaseña, ruteo comandas a mesas, producto, ruteo caja a mesas, producto

--- frontend/src/app/features/caja/caja.ts
+++ frontend/src/app/features/caja/caja.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-caja',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './caja.html',
   styleUrl: './caja.css',
 })
