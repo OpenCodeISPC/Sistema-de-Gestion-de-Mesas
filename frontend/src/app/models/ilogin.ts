85a5048c503c73e4f325950baeb348afab067edc
MilagrosCabrera23 <milagrosmagalycabrera@gmail.com>
2026-09-10 16:24:44 -0300

    add: agregamos la interfaz de login con auth y jwt

--- frontend/src/app/models/ilogin.ts
+++ frontend/src/app/models/ilogin.ts
@@ -1,7 +1,31 @@
-export interface ILogin {
+import { IUser } from "./iregistro";
 
+export type OauthProvider = 'google';
+
+// 1. DTO PARA LOGIN (POST - Lo que envías al backend)
+export interface ILoginRequest {
+    email: string;
+    password: string;
 }
 
+export interface IOauthRequest{
+    provider: OauthProvider;
+    token:string;
+}
+
+export type LoginPayload = ILoginRequest | IOauthRequest;
+
+// 2. RESPUESTA DEL LOGIN (Lo que devuelve Django con SimpleJWT/Token)
+
 export interface ILoginResponse {
- 
+    access: string;
+    refresh?: string;
+    user?: IUser;
+}
+// 3. REFRESCO DE TOKEN (Vital para el ciclo de vida de JWT)
+export interface IRefreshTokenRequest {
+    refresh: string;
+}
+export interface IRefreshTokenResponse {
+    access: string;
 }
